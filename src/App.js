044412d293d887fa69e6cdfe1df014a27fd89af5
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2021-03-25 23:13:02 +0200

    feat: show images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import first from './assets/first.jpg';
+import second from './assets/second.jpg';
+import third from './assets/third.jpg';
+import { canIShowImages } from './utils/getData'
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 function App() {
+  const [isLoad, setLoaded] = useState(false)
+  async function fetchData() {
+    const res = await canIShowImages();
+    if (!res) fetchData()
+    setLoaded(res);
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const images = [first, second, third];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='wrapper'>
+        {
+          isLoad ? images.map((el, i) => <img key={i} src={el} alt="mountains" />)
+            : images.map((_, i) => <Skeleton key={i} width={350} height={200} />)
+        }
+      </div>
     </div>
   );
 }
-
 export default App;
