151935763ae7eed925831a173b1708657503fdc4
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2021-04-02 13:52:36 +0300

    feat: optimized show images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import first from './assets/first.jpg';
 import second from './assets/second.jpg';
 import third from './assets/third.jpg';
-import { canIShowImages } from './utils/getData'
+import { canIShowImages } from './utils/getData';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
+function Images({ images, isLoad }) {
+  return isLoad
+    ? images.map((el, i) => <img key={i} src={el} alt='mountains' />)
+    : images.map((_, i) => <Skeleton key={i} width={350} height={200} />);
+}
+
 function App() {
-  const [isLoad, setLoaded] = useState(false)
+  const [isLoad, setLoaded] = useState(false);
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
   async function fetchData() {
     const res = await canIShowImages();
     setLoaded(res);
@@ -19,10 +25,7 @@ function App() {
   return (
     <div className='App'>
       <div className='wrapper'>
-        {
-          isLoad ? images.map((el, i) => <img key={i} src={el} alt="mountains" />)
-            : images.map((_, i) => <Skeleton key={i} width={350} height={200} />)
-        }
+        <Images images={images} isLoad={isLoad} />
       </div>
     </div>
   );
